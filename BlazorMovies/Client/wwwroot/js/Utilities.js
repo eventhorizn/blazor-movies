489b454bf4c7e2edb13e3be7a70e8ac2b0a7b115
Gary Hake <garyhake@gmail.com>
2021-11-16 07:51:57 -0500

    some additional changes to fix styling

---
+++ BlazorMovies/Client/wwwroot/js/Utilities.js
@@ -0,0 +1,31 @@
+﻿function my_function(message) {
+    console.log("from utilities" + message);
+}
+
+// Invoking static C# methods from js
+function dotNetStaticInvocation() {
+    DotNet.invokeMethodAsync("BlazorMovies.Client", "GetCurrentCount")
+        .then(result => {
+            console.log("count from javascript " + result);
+        });
+}
+
+// Invoking instance C# methods from js
+function dotNetInstanceInvocation(dotNetHelper) {
+    dotNetHelper.invokeMethodAsync("IncrementCount");
+}
+
+function initializeInactivityTimer(dotNetHelper) {
+    var timer;
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+
+    function resetTimer() {
+        clearTimeout(timer);
+        timer = setTimeout(logout, 1000 * 60 * 10); //10 minutes
+    }
+
+    function logout() {
+        dotNetHelper.invokeMethodAsync("Logout");
+    }
+}
